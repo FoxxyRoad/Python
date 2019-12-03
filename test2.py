9242781e07d5e928b819c4262ad2ec5252b49401
FoxxyRoad <58462830+FoxxyRoad@users.noreply.github.com>
2019-12-03 16:43:18 +0100

    Update test2.py

--- test2.py
+++ test2.py
@@ -20,23 +20,28 @@ def getoptions(args=sys.argv[1:]):
     zone of the instance.")
     parser.add_argument("-s", "--security_group", help="The security group of\
     the Instance.")
-    parser.add_argument("-tall", "--table_all", help="All tables")
+    parser.add_argument("-tall", "--table_all",  action='store_true',
+                        help="All tables")
     parser.add_argument("-tn", "--table_name", action='store_true',
                         help="Name")
-    parser.add_argument("-to", "--table_os", help="OS")
-    parser.add_argument("-tp", "--table_p", help="PatchDate")
-    parser.add_argument("-tip", "--table_ip", help="IP")
-    parser.add_argument("-td", "--table_d", help="Disk")
+    parser.add_argument("-to", "--table_os",  action='store_true', help="OS")
+    parser.add_argument("-tp", "--table_p",  action='store_true',
+                        help="PatchDate")
+    parser.add_argument("-tip", "--table_ip",  action='store_true', help="IP")
+    parser.add_argument("-td", "--table_d",  action='store_true', help="Disk")
     parser.add_argument("-tit", "--table_it", action='store_true',
                         help="Instance Type")
-    parser.add_argument("-ta", "--table_az", help="Availability zone")
-    parser.add_argument("-ts", "--table_sg", help="Security Group")
+    parser.add_argument("-ta", "--table_az",  action='store_true',
+                        help="Availability zone")
+    parser.add_argument("-ts", "--table_sg",  action='store_true',
+                        help="Security Group")
 
     options = parser.parse_args(args)
     return options
 
 
 options = getoptions(sys.argv[1:])
+
 
 filters = []
 
@@ -86,56 +91,90 @@ for security_group in vars(options):
             [options.security_group]}, )
         break
 
-output = ec2.describe_instances(Filters=filters)
-
-
-def filtern():
-    for reservation in output["Reservations"]:
-        for instance in reservation["Instances"]:
-
-            # Name
-            input_name = ""
-            for tags in instance["Tags"]:
-                if tags["Key"] == 'Name':
-                    input_name = tags["Value"]
-
-            # Betriebssystem
-            input_os = ""
-            for tags in instance["Tags"]:
-                if tags["Key"] == 'Betriebssystem':
-                    input_os = tags["Value"]
-
-            # PatchDate
-            input_pd = ""
-            for tags in instance["Tags"]:
-                if tags["Key"] == 'PatchDate':
-                    input_pd = tags["Value"]
-
-            # Groups
-            input_sg = ""
-            for group in instance["SecurityGroups"]:
-                input_sg += group["GroupName"]
-
-                if instance["SecurityGroups"][-1] != group:
-                    input_sg += " "
-
-            # Festplatten
-            input_fp = ""
-            for DeviceName in instance["BlockDeviceMappings"]:
-                input_fp += DeviceName["DeviceName"]
-
-                if instance["BlockDeviceMappings"][-1] != DeviceName:
-                    input_fp += " | "
-
-
 data = []
 table = terminaltables.AsciiTable(data)
+
+output = ec2.describe_instances(Filters=filters)
+
+for reservation in output["Reservations"]:
+    for instance in reservation["Instances"]:
+
+        # Name
+        input_name = ""
+        for tags in instance["Tags"]:
+            if tags["Key"] == 'Name':
+                input_name = tags["Value"]
+
+        # Betriebssystem
+        input_os = ""
+        for tags in instance["Tags"]:
+            if tags["Key"] == 'Betriebssystem':
+                input_os = tags["Value"]
+
+        # PatchDate
+        input_pd = ""
+        for tags in instance["Tags"]:
+            if tags["Key"] == 'PatchDate':
+                input_pd = tags["Value"]
+
+        # Groups
+        input_sg = ""
+        for group in instance["SecurityGroups"]:
+            input_sg += group["GroupName"]
+
+            if instance["SecurityGroups"][-1] != group:
+                input_sg += " "
+
+        # Festplatten
+        input_fp = ""
+        for DeviceName in instance["BlockDeviceMappings"]:
+            input_fp += DeviceName["DeviceName"]
+
+            if instance["BlockDeviceMappings"][-1] != DeviceName:
+                input_fp += " | "
+
 row = []
+title = []
 for table_name in vars(options):
     if options.table_name is not False:
-        data.append("Name")
-        row.append(filtern.input_name)
+        title.append('Name')
+        row.append(input_name)
         break
-
+for table_os in vars(options):
+    if options.table_os is not False:
+        title.append("Betriebssystem")
+        row.append(input_fp)
+        break
+for table_pd in vars(options):
+    if options.table_p is not False:
+        title.append("PatchDate")
+        row.append(input_pd)
+        break
+for table_ip in vars(options):
+    if options.table_ip is not False:
+        title.append("Private IP")
+        row.append(instance["PrivateIpAddress"])
+        break
+for table_d in vars(options):
+    if options.table_d is not False:
+        title.append("Festplatten")
+        row.append(input_fp)
+        break
+for table_instance in vars(options):
+    if options.table_it is not False:
+        title.append('Instance Typ')
+        row.append(instance["InstanceType"])
+        break
+for table_az in vars(options):
+    if options.table_az is not False:
+        title.append("Availability Zone")
+        row.append(instance["Placement"]["AvailabilityZone"])
+        break
+for table_sg in vars(options):
+    if options.table_sg is not False:
+        title.append("Security Group")
+        row.append(input_sg)
+        break
+data.append(title)
 data.append(row)
 print(table.table)
